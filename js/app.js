00b77f64c6c3f8c38b0932b0eee377bbea2911d0
PaolaFU <paola_sfu@hotmail.com>
2018-01-18 11:42:49 -0500

    Implementando la 2 vista, agregando imagenes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
-//Inicializando, el menú select de materialize
 $(document).ready(function() {
+    //Inicializando, el menú select de materialize
     $('select').material_select();
- });
+    //Inicializando, el carrusel de materialize
+    $('.carousel').carousel();
+    //Inicializando, el slider select de materialize
+    $('.slider').slider();
+  });
