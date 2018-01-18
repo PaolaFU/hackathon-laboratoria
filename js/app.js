826824c2a637cddee348559ab01bfe9936294b69
NatalyOC <noteroc@hotmail.com>
2018-01-17 23:41:46 -0500

    mplementando plugin autocomplete

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 //Inicializando, el menú select de materialize
 $(document).ready(function() {
     $('select').material_select();
+	alert('hi');
+  var $search = $('#input-search');
+  //implementando plugin autocomplete
+  // array para la lista de autocompletar
+  var tagsSuperHero = ['thor', 'superman', 'capitan america','a'];
+  //
+  $search.autocomplete({
+    source: tagsSuperHero
+  });	
+
  });
