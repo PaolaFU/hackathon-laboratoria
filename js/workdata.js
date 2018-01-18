27a4af31764922fb82704af0e8a4f3938c1d1cde
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2018-01-18 16:27:42 -0500

    Añadiendo carrusel con data dinámica

--- js/workdata.js
+++ js/workdata.js
@@ -6,17 +6,17 @@ $(document).ready(function() {
     var probando = information.Fotos;
     //recorriendo la data, para obtener fotos para el carrusel
     $.each(probando, function(key2,value2){
+      var div = $('<div>')
       var img = $('<img>');
       var Photo = value2.img; //me da la foto para el carrusel
-      console.log(Photo);
       img.attr('src',Photo);
+      div.attr('class','frame-k');
       var textPhoto = value2.texto; // me da el texto para el carrusel
-      $('#cont-img').append(img);
+      var x = div.append(img);
+      $('#slider-k').append(x);
       for (var i = 0; i < Photo.length; i++){
         var x = Photo.length[i];
-
       }
-
     });
 
   });
