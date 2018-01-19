b69f4e6baf31aa619d7c1644e22c3af8e8a4bbb7
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2018-01-19 14:35:24 -0500

    añadiendo estilos

--- js/workdata.js
+++ js/workdata.js
@@ -1,21 +1,24 @@
-$(document).ready(function() {
-  /*
+/*$(document).ready(function() {
+  var inputMovie = window.localStorage.getItem('character');
+
   //recorriendo data creada DC
   $.each(dc, function(key, value){
     var nameSuperHero = key;
-    var information = value;
-    var probando = information.Fotos;
-    //recorriendo la data, para obtener fotos para el carrusel
-    $.each(probando, function(key2,value2){
-      var img = $('<img>');
-      var Photo = value2.img; //me da la foto para el carrusel
-      img.attr('src',Photo);
-      img.attr('class', 'offset-s3 offset-l3 col s6 l6')
-      var textPhoto = value2.texto; // me da el texto para el carrusel
-      $('#m').append(img);
-      for (var i = 0; i < Photo.length; i++){
-        var x = Photo.length[i];
-      }
-    });
+    if inputMovie == key {
+      var information = value;
+      var probando = information.Fotos;
+      //recorriendo la data, para obtener fotos para el carrusel
+      $.each(probando, function(key2,value2){
+        var img = $('<img>');
+        var Photo = value2.img; //me da la foto para el carrusel
+        img.attr('src',Photo);
+        img.attr('class', 'offset-s3 offset-l3 col s6 l6')
+        var textPhoto = value2.texto; // me da el texto para el carrusel
+        $('#m').append(img);
+        for (var i = 0; i < Photo.length; i++){
+          var x = Photo.length[i];
+        }
+      });
+    }
   });*/
 });
