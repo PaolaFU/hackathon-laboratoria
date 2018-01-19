5127ed761e92ea31278690169476f7973927c14b
NatalyOC <noteroc@hotmail.com>
2018-01-18 23:24:33 -0500

    agregando funcionalidad a la vista marvel-home

--- js/app.js
+++ js/app.js
@@ -5,10 +5,23 @@ $(document).ready(function() {
   $('.carousel').carousel();
   // Inicializando, el slider select de materialize
   $('.slider').slider();
-
+ 
   $('#select-character').on('change', function() {
     var character = $('#select-character').val();
     window.localStorage.setItem('character', character);
     window.location.href = '../views/DCfilme.html';
   });
+  /* Funcionalidad para la vista Marvell-Home */
+  // Seleccionamos el elmento del DOM 'select'
+  var $selectCharacterM = $('#select-character-m');
+  // Obteniendo el valor de las opciones
+  $selectCharacterM.on('change',function() {
+    var $valueCharacter=$selectCharacterM.val();
+    // Almacena el valor de la opcion elegida
+    localStorage.character=$valueCharacter;
+    
+    // Redirecciona a la siguiente vista 
+    $(location).attr('href', '../views/marvel-personaje.html');
+  });
+
 });
