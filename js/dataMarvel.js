bfc3488afa8cc5afc694cdd671dc35513603fcd1
PaolaFU <paola_sfu@hotmail.com>
2018-01-19 13:27:41 -0500

    Modificando data de dataMarvel.js

--- js/dataMarvel.js
+++ js/dataMarvel.js
@@ -91,3 +91,39 @@ var marvel = {
     },
   },
 },
+var personajesM = [
+  {
+    name: 'Iron Man',
+    photo: ['<img src=\'http://www.optionated.com/wp-content/uploads/2012/06/Iron-Man-1371.jpg\'>',
+      '<img src=\'https://vignette.wikia.nocookie.net/marveldatabase/images/6/66/Iron_Man_2_%28film%29_0001.jpg/revision/latest?cb=20100423181955\'>', 
+      '<img src=\'http://www.twitrcovers.com/wp-content/uploads/2012/11/Iron-Man-III-l.jpg\'>'],
+    caracteristica: ['Iron Man, cuya identidad pública es la de Anthony "Tony" Stark , un científico y empresario estadounidense, fabricante de armas'],
+    curiosidad: ['Cambios no tan radicales, dentro de su aparción cinematográfica, pero con un gran giro en THor ragnarok',
+    'Primera aparición: Tales of Suspense #39, (1963) Con esta armadura empezó todo; su apariencia tosca y fría generaba miedo en sus primeros enemigos.',
+    'Días Dorados, el primer cambio de muchos que vendrían en su historia: cambiarle el color. ',
+    'Iron Man, a armadura está dentro de él además de que su cerebro trabaja como una gran computadora almacenadora de datos; pre-visualiza los acontecimientos como nadie, golpea antes de que los demás actúen; puede conectarse al sistema de cualquier computadora.']
+    
+  },
+  {
+    name: 'Thor',
+    photo: ['<img src=\'https://bdn-data.s3.amazonaws.com/uploads/2011/05/76031_gal-600x400.jpg\'>',
+      '<img src=\'https://sillykhan.files.wordpress.com/2013/11/thor-1.jpg\'>', 
+      '<img src=\'https://k61.kn3.net/taringa/B/6/C/7/6/4/everhard2000/931.jpg\'>'],
+    caracteristica: ['Thor Odinson es un príncipe-guerrero Asgardiano, el Semi-dios del Trueno, y un protector auto-proclamado de la Tierra.'],
+    curiosidad: ['A lo largo de las apariciones cinematogrpaficas, de Wonder Woman, se puede apreciar un cambio radiacal en el vestuario',
+    'Este traje, es mejor el acabado y con mayor notoriedad en las expresiones y en la armadura. ',
+    'Thor ragnarok: Thor perderá su mítica melena y cambiará su clásica armadura, ahora gracias a un concept-art se verá mejor']
+    
+  },
+  {
+    name: 'Capitan America',
+    photo: ['<img src=\'http://img2.wikia.nocookie.net/__cb20120512183255/marvel-movies/es/images/f/f7/Capit%C3%A1n-Am%C3%A9rica-El-Primer-Vengador.jpg\'>',
+      '<img src=\'https://pre00.deviantart.net/b2f0/th/pre/i/2014/125/e/f/captain_america_winter_soldier_by_billycsk-d7hay6o.jpg\'>', 
+      '<img src=\'https://blogdesuperheroes.es/wp-content/plugins/BdSGallery/BdSGaleria/37530.jpg\'>'],
+    caracteristica: ['amuel Wilson se convirtió en el nuevo Capitán América a consecuencia de que Steve Rogers envejeciera al extraerle el suero del supersoldado por Iron Nail.'],
+    curiosidad:['Cambios en las diferentes épocas del personaje; colores diferentes, formas de escudo totalmente distintas, con casco, sin casco, etc. ',
+    'Capitan america el primer vengador: El traje estaba hecho de polímero de carbono y ofrecía una mediana resistencia a las balas y las armas blancas. También le permitía colgar su escudo en la espalda.',
+    'Capitan america winter soldier: Tiene tonos más oscuros, luce más utilitario y le permite una mayor movilidad. Además, su escudo se adhiere a su espalda magnéticamente. ']
+    
+  },
+  
